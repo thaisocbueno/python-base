091d7cc346717240ddca6d558b8997d53b1cc998
Thais Bueno <thaisocbueno@gmail.com>
2024-07-08 14:09:25 -0300

    Melhorias no Algoritmo Tabuada

--- tabuada.py
+++ tabuada.py
@@ -1,15 +1,27 @@
 #! /usr/bin/env python3
-"""Imprime a tabuada do 1 a 10."""
-__versao__ = "0.0.1"
+"""Imprime a tabuada do 1 a 10.
+Exemplo da impressao das tabuadas:
+---Tabuada do 1---
+    1 x 1 = 1
+    1 x 2 = 2
+    1 x 3 = 3
+...
+##################
+---Tabuada do 2---
+    2 x 1 = 2
+    2 x 2 = 4
+...
+"""
+__versao__ = "0.1.1"
 __autor__ = "Thais Bueno"
 
-#Range inicia no 0
-numeros_tabuada = list(range(0, 11))
+numeros_tabuada = list(range(1, 11))
 
-for numero_tabuada in numeros_tabuada:
-    print("Tabuada do: ", numero_tabuada)
-    for numero_multiplicado in numeros_tabuada:
-        resultado = numero_tabuada * numeros_tabuada[numero_multiplicado]
-        print(numero_tabuada, " x ", numeros_tabuada[numero_multiplicado], 
-              " = ", resultado)
-    print("-" * 12)
+for tabuada in numeros_tabuada:
+    print("{:-^18}".format(f"Tabuada do {tabuada}"))
+    print()
+    for multiplicador in numeros_tabuada:
+        resultado = tabuada * multiplicador
+        print("{:^18}".format(f"{tabuada} x {multiplicador} = {resultado}"))
+    print()
+    print("#" * 18)
